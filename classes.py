956ceea91707f178ad0c63407fa19d032a3c5ece
Eddie Paes <eddiepaes2204@gmail.com>
2022-05-22 20:11:52 -0300

    Alterando nomes.

--- classes.py
+++ classes.py
@@ -7,52 +7,42 @@ e definições pertinentes ao algoritmo
 class Node:
     def __init__(self, dado):
         self.dado = dado
-        self.next = None
-        self.prev = None
+        self.next = None # ponteiro pro próximo
+        self.prev = None # ponteiro pro anterior
 
 
 class Lista_Dupla:
     def __init__(self):
-        self.head = None
-        self.tail = None
+        self.head = None # inicio
+        self.tail = None # final
         self.size = 0
 
 
-
     def vazia(self):
-        if self.inicio is None:
+        if self.head is None:
             return True
         else:
             return False
 
-    def adicionar_inicio(self, valor):
-        no = No(valor)
+    def add_head(self, valor):
+        no = Node(valor)
         if self.vazia():
-            self.inicio = self.fim = no
+            self.head = self.tail = no
         else:
-            no.proximo = self.inicio
-            self.inicio.anterior = no
-            no.anterior = None
-            self.inicio = no
-        self.tamanho += 1
+            no.next = self.head
+            self.head.prev = no
+            no.prev = None
+            self.head = no
+        self.size += 1
 
-    def adicionar_fim(self, valor):
-        no = No(valor)
+    def add_tail(self, valor):
+        no = Node(valor)
         if self.vazia():
-            self.inicio = self.fim = no
+            self.head = self.tail = no
         else:
-            self.fim.proximo = no
-            no.anterior = self.fim
-            no.proximo = None
-            self.fim = no
-        self.tamanho += 1
-
-
-
-
-
-    def calcular(self,list1,list2):
-        # todo implementar o calculo ja com as listas em formas numericas
-        pass
-
+            self.tail.next = no
+            no.prev = self.tail
+            no.next = None
+            self.tail = no
+        self.size += 1
 
--- functions.py
+++ functions.py
@@ -1,5 +1,6 @@
 from classes import *
 
+
 def separador(string):
     list = []
     counter = 0
@@ -9,6 +10,15 @@ def separador(string):
     list += [string[i:i + 2] for i in range(counter, len(string), 2)]
     return string.converter(list)
 
-def produto(self,fator1,fator2):
-    #TODO implement
-    pass
+
+def produto(self, ftr1, ftr2):
+    # Converter a string recebida divida numa lista encadeada
+    lista_fator1 = listar(ftr1)
+    lista_fator2 = listar(ftr2)
+
+
+def listar(self, lst):
+    lista_dupla = Lista_Dupla()
+    for i in lst:
+        lista_dupla.add_tail(i)
+    return lista_dupla
