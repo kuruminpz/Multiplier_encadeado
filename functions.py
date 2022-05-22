72f7763ed087158d8fde352f0607be86ea529719
Eddie Paes <eddiepaes2204@gmail.com>
2022-05-22 20:42:53 -0300

    Salvando Modificações no código.

--- functions.py
+++ functions.py
@@ -11,14 +11,34 @@ def separador(string):
     return string.converter(list)
 
 
-def produto(self, ftr1, ftr2):
-    # Converter a string recebida divida numa lista encadeada
-    lista_fator1 = listar(ftr1)
-    lista_fator2 = listar(ftr2)
-
-
 def listar(self, lst):
     lista_dupla = Lista_Dupla()
     for i in lst:
         lista_dupla.add_tail(i)
     return lista_dupla
+
+
+def produto(self, ftr1, ftr2):
+    # Converter a string recebida divida numa lista encadeada
+    lista_fator1 = listar(ftr1)
+    lista_fator2 = listar(ftr2)
+
+    final1 = lista_fator1.tail
+    final2 = lista_fator2.tail
+    final1_result = None
+    final2_result = None
+
+    over = 0
+    result = 0
+    num1 = 0
+    num2 = 0
+    num_result = ""
+
+    while final1 != None:
+        final2 = lista_fator2.tail
+        seg1 = int(final1.dado) # Seg == segmento, referenciando parte do fator
+        final2_result = final1_result
+        while final2 != None:
+            seg2 = int(final2.dado)
+            result = seg1 * seg2
+
